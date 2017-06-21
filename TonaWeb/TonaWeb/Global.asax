53bbfcab1ecae2571633339829db6c766fe0c7c8
gustifeli <gustifeli@hotmail.com>
2017-06-20 22:00:23 -0300

    start project

---
+++ TonaWeb/TonaWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TonaWeb.MvcApplication" Language="C#" %>
---
+++ TonaWeb/TonaWeb/Models/MailModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TONA.Models
+{
+    public class MailModel
+    {
+        //public string From {get; set; }
+        //public string To { get; set; }
+        //public string Subjet { get; set; }
+        //public string Body { get; set; }
+
+
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public string telefono { get; set; }
+        public string localidad { get; set; }
+        public string comentario { get; set; }
+    }
+}
